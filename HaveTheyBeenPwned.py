63e2db96bfe681eacf501956363b0171995fbb83
phyushin <phyushin@gmail.com>
2019-05-17 14:54:47 +0100

    added colour to term output, also cleaned up mixed tabs and spaces

--- HaveTheyBeenPwned.py
+++ HaveTheyBeenPwned.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 ##
-# HaveTheyBeenPwned, bulk HaveIBeenPwned scraper, this script is a modifcation of https://github.com/Techno-Hwizrdry/checkpwnedemails
+# HaveTheyBeenPwned, bulk HaveIBeenPwned scraper, this script is a modifcation
+# of https://github.com/Techno-Hwizrdry/checkpwnedemails
 ##
-__author__  = "Chris Burton"
-__version__ = "1.3"
+__author__ = "Chris Burton"
+__author__ = "Paul Williams"
+__version__ = "1.4"
 
 from argparse import ArgumentParser
-from time     import sleep
+from time import sleep
 
 import time
 import json
@@ -22,150 +24,178 @@ HEADERS = {"User-Agent": "havetheybeenpwned"}
 
 EMAILINDEX = 0
 PWNEDINDEX = 1
-DATAINDEX  = 2
+DATAINDEX = 2
 
 BREACHED = "breachedaccount"
 PASTEBIN = "pasteaccount"
+
+# simple terminal colour
+
+
+class term_colour:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def print_message(colour, bullet_point, message):
+    print colour + "[" + bullet_point + "] " + term_colour.ENDC + message
+
+
+def print_debug(message):
+    print_message(term_colour.OKBLUE, "i", message)
+
+
+def print_info(message):
+    print_message(term_colour.OKGREEN, "+", message)
+
+
+def print_warning(message):
+    print_message(term_colour.FAIL, "!", message)
+
 
 def indent(lines, amount, ch=' '):
     padding = amount * ch
     return padding + ('\n'+padding).join(lines.split('\n'))
 
+
 class PwnedArgParser(ArgumentParser):
-	def error(self, message):
-		sys.stderr.write('error: %s\n' %message)
-		self.print_help()
-		sys.exit(2)
+    def error(self, message):
+        sys.stderr.write('error: {0}\n'.format(message))
+        self.print_help()
+        sys.exit(2)
 
 
 def get_args():
-	parser = PwnedArgParser()
+    parser = PwnedArgParser()
+    parser.add_argument('-i', dest='input_path', help='Path to text file that lists email addresses.')
+    parser.add_argument('-o', dest='output_path', help='Path to output to text file.')
+    parser.add_argument('-oR', dest='output_path_report', help='Path to output to text file in report ready format.')
+    parser.add_argument('-s', dest='rate_limit_sleep', help='Set a wait time between each request (seconds). Default of 1.6 seconds - set to 0 for no wait.', default=1.6, type=float)
+    if len(sys.argv) == 1:  # If no arguments were provided, then print help and exit.
+        parser.print_help()
+        sys.exit(1)
 
-	parser.add_argument('-i', dest='input_path',   help='Path to text file that lists email addresses.')
-	parser.add_argument('-o', dest='output_path',  help='Path to output to text file.')
-	parser.add_argument('-oR', dest='output_path_report',  help='Path to output to text file in report ready format.')
-	parser.add_argument('-s', dest='rate_limit_sleep',  help='Set a wait time between each request (seconds). Default of 1.6 seconds - set to 0 for no wait.', default=1.6, type=float)
+    return parser.parse_args()
 
-	if len(sys.argv) == 1:  # If no arguments were provided, then print help and exit.
-		parser.print_help()
-		sys.exit(1)
-
-	return parser.parse_args()
 
 #  Used for removing the trailing '\n' character on each email.
 def clean_list(list_of_strings):
-	return [str(x).strip() for x in list_of_strings]
+    return [str(x).strip() for x in list_of_strings]
+
 
 def get_results(email_list, service, opts):
-	results = []  # list of tuples (email adress, been pwned?, json data)
-	for email in email_list:
-		email = email.strip()
-		data = []
-		req  = urllib2.Request(PWNED_API_URL % (urllib.quote(service), urllib.quote(email)), headers=HEADERS)
+    results = []  # list of tuples (email adress, been pwned?, json data)
+    for email in email_list:
+        email = email.strip()
+        data = []
+        req = urllib2.Request(PWNED_API_URL % (urllib.quote(service), urllib.quote(email)), headers=HEADERS)
+        try:
+            response = urllib2.urlopen(req)  # This is a json object.
+            data = json.loads(response.read())
+            results.append((email, True, data))
+        except urllib2.HTTPError as e:
+            if e.code == 400:
+                print_warning("{0} does not appear to be a valid email address. HTTP Error 400.".format(email))
+            if e.code == 403:
+                print_warning("Forbidden - no user agent has been specified in the request.  HTTP Error 403.")
+            if e.code == 404:
+                results.append((email, False, data))
+            if e.code == 429:
+                print_warning("Too many requests; going over the request rate limit, sleeping for 1.6 seconds.")
+            sleep(1.6)
 
-                try:
-                	response = urllib2.urlopen(req)  # This is a json object.
-                        data     = json.loads(response.read())
-			results.append( (email, True, data) )
-                except urllib2.HTTPError as e:
-                        if e.code == 400:
-				print "%s does not appear to be a valid email address.  HTTP Error 400." % (email)
-			if e.code == 403:
-				print "Forbidden - no user agent has been specified in the request.  HTTP Error 403."
-                        if e.code == 404:
-				results.append( (email, False, data) )
-			if e.code == 429:
-				print "Too many requests; going over the request rate limit, sleeping for 1.6 seconds."
-				sleep (1.6)
+        if opts.rate_limit_sleep:
+            sleep(float(opts.rate_limit_sleep))
 
-		if opts.rate_limit_sleep:
-			sleep(float(opts.rate_limit_sleep))
+        try:
+            last_result = results[-1]
+            if not last_result[PWNEDINDEX]:
+                if service == BREACHED:
+                    pass
+                else:
+                    pass
+            elif data:
+                found_email = []
+                last_item = 0
+                for i in data:
+                    found_email.append(i['Title'])
+                    found_in_list = ', '.join(found_email)
+                    # This is what gets put into the file
+                    output_string = ("{0} ({1})\r\n".format(email, found_in_list))
 
-		#if not opts.output_path:
-		try:
-			last_result = results[-1]
+                print_debug("found_email:{}".format(found_email))
+                print_debug("email:{0}\r\nFound in list{1}".format(email, found_in_list))
+                print_warning("{0} ({1})".format(email, found_in_list))
 
-			if not last_result[PWNEDINDEX]:
-				if service == BREACHED:
-					pass
-				else:
-					pass
-			elif data:
-				found_email = []
-				last_item = 0
-				sys.stdout.write(('%s (' % (email)))
-				for i in data:
-					found_email.append( i['Title'] )
+                if opts.output_path:
+                    with open(opts.output_path, "a") as output_file:
+                        output_file.write(output_string)
 
-				sys.stdout.write(','.join(found_email))
-				sys.stdout.write(')')
-				outputString = (('%s' %email + ' (' + ', '.join(found_email) + ')\r'))
-				sys.stdout.flush()
-				print ''
+                if opts.output_path_report:
+                    with open(opts.output_path_report, "a") as output_file:
+                        output_file.write("- " + output_string)
 
-				if opts.output_path:
-					with open(opts.output_path, "a") as outputFile:
-						outputFile.write(outputString)
+        except IndexError:
+            pass
 
-				if opts.output_path_report:
-					with open(opts.output_path_report, "a") as outputFile:
-						outputFile.write("- "+ outputString)
+    return results
 
 
-		except IndexError:
-			pass
+def file_len(fname, sleeptime):
+    with open(fname) as f:
+        for ii, l in enumerate(f):
+            ii + 1
+            pass
 
-	return results
+    total_time = sleeptime * ii / 60
+    print_info("{0} will take approx {1} minutes.".format(fname, str(total_time)))
 
-def file_len(fname,sleeptime):
-	with open(fname) as f:
-		for ii, l in enumerate(f):
-			ii + 1
-			pass
-	total_time = sleeptime * ii / 60
-	print ('[+] ' + fname + ' will take approx ' + str(total_time) + ' minutes.')
 
 #  This function will convert every item, in dlist, into a string and
 #  encode any unicode strings into an 8-bit string.
 def clean_and_encode(dlist):
-	cleaned_list = []
+    cleaned_list = []
+    for d in dlist:
+        try:
+            cleaned_list.append(str(d))
+        except UnicodeEncodeError:
+            cleaned_list.append(str(d.encode('utf-8')))  # Clean the data.
+    return cleaned_list
 
-	for d in dlist:
-		try:
-			cleaned_list.append(str(d))
-		except UnicodeEncodeError:
-			cleaned_list.append(str(d.encode('utf-8')))  # Clean the data.
-
-	return cleaned_list
 
 def write_results_to_file(filename, results, opts):
-	pass
+    pass
+
 
 def main():
-	email_list = []
-	opts = get_args()
+    email_list = []
+    opts = get_args()
 
-	file_len(opts.input_path,opts.rate_limit_sleep)
-	email_list_file = open(opts.input_path, 'r')
-	email_list      = clean_list(email_list_file.readlines())
+    file_len(opts.input_path, opts.rate_limit_sleep)
+    email_list_file = open(opts.input_path, 'r')
+    email_list = clean_list(email_list_file.readlines())
+    email_list_file.close()
 
-	email_list_file.close()
-
-	if opts.output_path:
-		print "[+] Saving to file " + str(opts.output_path)
-	if opts.output_path_report:
-		print "[+] Saving to file " + str(opts.output_path_report)
-	if os.path.isfile(opts.input_path):
-		print "[+] Querying HaveIBeenPwned!"
-
+    if opts.output_path:
+        print_info("Saving to file {0}".format(str(opts.output_path)))
+    if opts.output_path_report:
+        print_info("Saving to file {0}".format(str(opts.output_path_report)))
+    if os.path.isfile(opts.input_path):
+        print_info("Querying HaveIBeenPwned!")
         results = []
 
-	results.append(get_results(email_list, BREACHED, opts))
-	results.append(get_results(email_list, PASTEBIN, opts))
+    results.append(get_results(email_list, BREACHED, opts))
+    results.append(get_results(email_list, PASTEBIN, opts))
+    if opts.output_path:
+        pass
+    print_info("Finished!")
 
-
-	if opts.output_path:
-		pass
 
 if __name__ == '__main__':
-	main()
+    main()
